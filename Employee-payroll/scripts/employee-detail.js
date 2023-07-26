9c8eae93f99bdb89ac0ecca020f42e9cf091b161
unknown <sachinkaythamwar@gmail.com>
2023-07-26 11:35:22 +0530

    Modified and added Cancel and reset functionality.

--- Employee-payroll/scripts/employee-detail.js
+++ Employee-payroll/scripts/employee-detail.js
@@ -1,20 +1,31 @@
-let data;
+
 function displayData() {
     $.ajax({
         url: 'http://localhost:3000/employees',
         type: 'GET',
         success: function (data) {
+            // console.log(data);
             for(let i = 0; i < data.length; i++) {
                 let employee = data[i];
+                // console.log(employee);
+                let departments = JSON.parse(employee.department);
                 const newRow = document.createElement('tr');
+                newRow.id = employee.id;
                 newRow.innerHTML = `
                 <td style="text-align: right; width: 35px;"><img src="${employee.imageUrl}"></td>
                 <td>${employee.name}</td>
                 <td>${employee.gender}</td>
-                <td>${employee.department}</td>
+                <td>
+                    <span class='department-data'>${departments}</span>
+                </td>
                 <td>${employee.salary}</td>
                 <td>${employee.startDate}</td>
-                <td></td>
+                <td>
+                    <div class="actions">
+                        <i class="fa-solid fa-trash-can" onclick="deleteItem(${employee.id})"></i>
+                        <i class="fa-solid fa-pen" style="margin-left: 15px;" onclick="editEmployee(${employee.id})"></i>
+                    </div>
+                </td>
                 `
                 const tableBody = document.getElementById('employeeData');
                 tableBody.appendChild(newRow);
@@ -26,3 +37,22 @@ function displayData() {
     });
 }
 displayData();
+
+function deleteItem(id) {
+    $.ajax({
+        url: `http://localhost:3000/employees/${id}`,
+        type: 'DELETE',
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+
+function editEmployee(id) {
+    localStorage.setItem('employee', id);
+    window.location.href = 'http://127.0.0.1:5500/Employee-payroll/pages/payroll-form.html';
+}
--- Employee-payroll/scripts/payroll-form.js
+++ Employee-payroll/scripts/payroll-form.js
@@ -1,41 +1,37 @@
-let oddMonth = document.getElementById('oddMonth'); 
-let month = document.getElementById('month').value;
+let id = localStorage.getItem('employee');
+let name = document.getElementById('name');
+let img = document.getElementsByName('image');
+let gender1 = document.querySelector('#male');
+let gender2 = document.querySelector('#female');
+let checkbox = document.getElementsByName('department');
+let salary = document.getElementById('select-salary');
+let day = document.getElementById('day');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let notes = document.getElementById('notes');
+let gender;
 let imageUrl;
+let selectedDepartment = [];
+let submitButton = document.getElementsByClassName('submit-btn')[0];
+let updateButton = document.getElementsByClassName('update-btn')[0];
 
-// document.getElementById('day').addEventListener('change', function() {
-//     var selectedDay = this.value;
-//     if(selectedMonth && selectedDay === '31') {
-
-//     }
-// });
-
-// document.getElementById('month').addEventListener('change', function() {
-//     var selectedMonth = this.value;
-//     if (selectedMonth === 'January' || selectedMonth === 'March' || selectedMonth === 'May' || selectedMonth === 'July' || selectedMonth === 'August' || selectedMonth === 'October' || selectedMonth === 'December') {
-//         oddMonth.style.display = 'none';
-//     } else {
-//         oddMonth.style.display = 'block';
-//     }
-// });
-
-// document.getElementById('year').addEventListener('change', function() {
-//     var selectedMonth = this.value;
-//     console.log('Selected month:', selectedMonth);
-// });
+if(localStorage.getItem('employee')) {
+    getData();
+    submitButton.style.display = 'none';
+    updateButton.style.display = 'block';
+} else {
+    submitButton.style.display = 'block';
+    updateButton.style.display = 'none';
+}
 
 
 function onSubmit(event) {
-    event.preventDefault();
-    let name = document.getElementById('name').value;
-    let img = document.getElementsByName('image');
-    let gender1 = document.querySelector('#male');
-    let gender2 = document.querySelector('#female');
-    let checkbox = document.getElementsByName('department');
-    let salary = document.getElementById('select-salary').value;
-    let day = document.getElementById('day').value;
-    let month = document.getElementById('month').value;
-    let year = document.getElementById('year').value;
-    let notes = document.getElementById('notes').value;
+    let name = name.value;
+    let salary = salary.value;
+    let day = day.value;
+    let month = month.value;
+    let year = year.value;
+    let notes = notes.value;
     
     for (let i = 0; i < img.length; i++) {
         if (img[i].checked) {
@@ -57,31 +53,119 @@ function onSubmit(event) {
             selectedDepartment.push(checkbox[i].value);
         }
     }
-
-    if(selectedDepartment.length > 0) {
-        selectedDepartment.join(',');
-    }
-    console.log(selectedDepartment)
+    selectedDepartment.sort().reverse();
+    
     const data = {
         name: name,
         imageUrl: imageUrl,
         gender:gender,
-        department:selectedDepartment,
+        department:JSON.stringify(selectedDepartment),
         salary: salary,
         startDate: `${day} ${month} ${year}`, 
         notes:notes
     }
     console.log(data);
     
+    // setTimeout(() => {
+        $.ajax({
+            url: 'http://localhost:3000/employees',
+            type: 'POST',
+            data: data,
+            success: function (data) {
+                console.log(data);
+            },
+            // error: function (error) {
+            //     console.log(error);
+            // }
+        });
+    // }, 2000);
+}
+
+function getData() {
     $.ajax({
-        url: 'http://localhost:3000/employees',
-        type: 'POST',
-        data: data,
+        url: `http://localhost:3000/employees/${id}`,
+        type: 'GET',
         success: function (data) {
-            console.log(data);
+            setTimeout(() => {               
+                name.value = data.name;
+                for(let i = 0; i < img.length; i++) {
+                    if(data.imageUrl === img[i].value) {
+                        img[i].checked = true;
+                        break;
+                    }
+                } 
+                if(data.gender === 'Male') {
+                    gender = gender1.value;
+                    gender1.checked = true;
+                } else { 
+                    gender = gender2.value;
+                    gender2.checked = true;
+                }
+                let depart = JSON.parse(data.department);
+                for(let i = 0; i < depart.length; i++) {
+                    for(let j = 0; j < checkbox.length; j++) {
+                        if (depart[i] === checkbox[j].value) {
+                            checkbox[j].checked = true;
+                            break;
+                        }
+                    }
+                }
+                for(let i = 0; i < checkbox.length; i++) {
+                    if(checkbox[i].checked) {
+                        selectedDepartment.push(checkbox[i].value);
+                    }
+                }
+                selectedDepartment.sort().reverse();
+                salary.value = data.salary;
+                day.value = data.startDate.split(' ')[0];
+                month.value = data.startDate.split(' ')[1];
+                year.value = data.startDate.split(' ')[2];
+                notes.innerHTML = data.notes;
+            }, 200);
         },
         // error: function (error) {
         //     console.log(error);
         // }
     });
 }
+
+function updateData(event) {
+    event.preventDefault();
+    const data = {
+        name: name.value,
+        imageUrl: imageUrl,
+        gender:gender,
+        department:JSON.stringify(selectedDepartment),
+        salary: salary.value,
+        startDate: `${day.value} ${month.value} ${year.value}`, 
+        notes:notes.value
+    }
+    console.log(data);
+    $.ajax({
+        url: `http://localhost:3000/employees/${id}`,
+        type: 'PUT',
+        data: data,
+        success: function (data) {
+            console.log(data);
+            localStorage.removeItem('employee');
+            onReset();
+            window.location.href = 'http://127.0.0.1:5500/Employee-payroll/pages/employee-detail.html'
+        },
+        // error: function (error) {
+        //     console.log(error);
+        // }
+    });
+}
+
+
+function onCancel() {
+    window.history.go(-1);
+}
+
+function onReset(event) {
+    event.preventDefault();
+    let form = document.getElementById('form');
+    form.reset();
+}
+
+
