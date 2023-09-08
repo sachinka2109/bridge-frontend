cefe0761e8b0e0a987573d0f01f977658ac78f8f
sachinka2109 <101002628+sachinka2109@users.noreply.github.com>
2023-09-08 15:09:41 +0530

    Update:Minor update in Auth Route

--- React/fundo/src/router/AuthRoute.jsx
+++ React/fundo/src/router/AuthRoute.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Navigate,useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 let AuthRoute = ({children}) => {
-    const location = useLocation(); 
     const token = localStorage.getItem('token');
     if(!token) {
         // console.log('this is  auth route')
         return children
     }
-    return <Navigate to='/dashboard' replace state={{ from: location }}/>
+    return <Navigate to='/dashboard' replace/>
 }
 
 export default AuthRoute
